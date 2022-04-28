6191f22da1b207d5ae10dc156d9bf58bfb8d2121
172454 <68299663+172454@users.noreply.github.com>
2022-04-28 14:36:34 +0200

    Updated for Python 3

--- src/main_enhancement.py
+++ src/main_enhancement.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+# Run: python main_enhancement.py "..\images\StockProcessed.png"
+
 import numpy as np
 import matplotlib.pylab as plt;
 import scipy.ndimage
@@ -7,16 +9,32 @@ import sys
 import imageio
 import matplotlib
 
+import PIL ##
+
 from image_enhance import image_enhance
+
+from PIL import Image ##
 
 if(len(sys.argv)<2):
     print('loading sample image');
     img_name = '1.jpg'
-    img = scipy.ndimage.imread('../images/' + img_name);
+#    img = scipy.ndimage.imread('../images/' + img_name);
+    img = imageio.imread('../images/' + img_name); ##
 elif(len(sys.argv) >= 2):
     img_name = sys.argv[1];
-    img = scipy.ndimage.imread(sys.argv[1]);
-    
+#    img = scipy.ndimage.imread(sys.argv[1]);
+    img = imageio.imread(sys.argv[1]); ##
+
+if(len(sys.argv) >= 3):
+    output_name = sys.argv[2];
+else:
+    output_name = '../enhanced/' + 'enhanced.png';
+
+
+# Just a debug check if anything is even happening:
+# # matplotlib.image.imsave('../enhanced/' + 'copy-of-input.png', img) ##
+# matplotlib.image.imsave('g:/Work/Fingerprints/Fingerprint-Enhancement-Python-master/images/' + 'copy-of-input.png', img) ##
+
 if(len(img.shape)>2):
     img = np.dot(img[...,:3], [0.299, 0.587, 0.114]);
 
@@ -26,12 +44,16 @@ aspect_ratio = np.double(rows)/np.double(cols);
 new_rows = 350;             #randomly selected number
 new_cols = new_rows/aspect_ratio;
 
-img = scipy.misc.imresize(img,(np.int(new_rows),np.int(new_cols)));
+#img = scipy.misc.imresize(img,(np.int(new_rows),np.int(new_cols)));
+#img = np.array(Image.fromarray(img).resize((int(new_cols), int(new_rows)), Image.LANCZOS)) ##
 
-enhanced_img = image_enhance(img);    
+enhanced_img = image_enhance(img);
 
 if(1):
-    print('saving the image')
-    matplotlib.image.imsave('../enhanced/' + 'enhanced.jpg', enhanced_img)
+#    print('saving the image')
+#    matplotlib.image.imsave('../enhanced/' + 'enhanced.jpg', enhanced_img)
+#    matplotlib.image.imsave('../enhanced/' + 'enhanced.png', enhanced_img) ##
+    matplotlib.image.imsave(output_name, enhanced_img) ##
 else:
     plt.imshow(enhanced_img,cmap = 'Greys_r');
+
--- src/ridge_filter.py
+++ src/ridge_filter.py
@@ -71,18 +71,21 @@ def ridge_filter(im, orient, freq, kx, ky):
     sigmax = 1/unfreq[0]*kx;
     sigmay = 1/unfreq[0]*ky;
     
-    sze = np.round(3*np.max([sigmax,sigmay]));
-    
+#    sze = np.round(3*np.max([sigmax,sigmay]));
+    sze = int(np.round(3*np.max([sigmax,sigmay]))); ##
+
     x,y = np.meshgrid(np.linspace(-sze,sze,(2*sze + 1)),np.linspace(-sze,sze,(2*sze + 1)));
     
     reffilter = np.exp(-(( (np.power(x,2))/(sigmax*sigmax) + (np.power(y,2))/(sigmay*sigmay)))) * np.cos(2*np.pi*unfreq[0]*x); # this is the original gabor filter
     
     filt_rows, filt_cols = reffilter.shape;    
     
-    gabor_filter = np.array(np.zeros((180/angleInc,filt_rows,filt_cols)));
-    
-    for o in range(0,180/angleInc):
-        
+#    gabor_filter = np.array(np.zeros((180/angleInc,filt_rows,filt_cols)));
+    gabor_filter = np.array(np.zeros((int(180/angleInc),filt_rows,filt_cols))); ##
+
+#    for o in range(0,180/angleInc):
+    for o in range(0,int(180/angleInc)): ##
+
         # Generate rotated versions of the filter.  Note orientation
         # image provides orientation *along* the ridges, hence +90
         # degrees, and imrotate requires angles +ve anticlockwise, hence
